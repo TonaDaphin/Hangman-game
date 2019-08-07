f92a23030171361a1a048b0be3380b3f8d69b7d6
ktscates <cates2506@gmail.com>
2019-08-07 10:46:14 +0200

    Add functions in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,24 @@ var animals=["lion","tiger","zebra","girafe","peguin"];
 var cars=["mercedes","polo","honda","jeep","bmw"];
 var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var word;
+var answerArray=[]
 
 function animal(){
-    word=animals[Math.floor(Math.random()*animals.length)];
-    word=word.replace(/\s/g, "-");
+    word= document.getElementById("hold").innerHTML; 
+    word=animals[Math.floor(Math.random()*animals.length)];  
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }  
+    document.getElementById("hold").innerHTML = answerArray;
+    console.log(word);
+}
+
+function car(){
+    word= document.getElementById("hold").innerHTML; 
+    word=cars[Math.floor(Math.random()*cars.length)];  
+    for (var i = 0; i < word.length; i++) {
+        answerArray[i] = "_";
+    }  
+    document.getElementById("hold").innerHTML = answerArray;
     console.log(word);
 }
