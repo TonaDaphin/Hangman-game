a83c98fff1af0cfcbb3f4ac4b82fefb5d2bce284
ktscates <cates2506@gmail.com>
2019-08-10 17:08:20 +0200

    Update js file

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-var animals=["lion","tiger","zebra","girafe","peguin"];
-var cars=["mercedes","polo","honda","jeep","bmw"];
+
+var animals=["peguin"];
+var cars=["mercedes"];
 var alphabet=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var word;
 var answerArray=[]
+
+
 
 function animal(){
     word= document.getElementById("hold").innerHTML; 
@@ -10,7 +13,7 @@ function animal(){
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = "_";
     }  
-    document.getElementById("hold").innerHTML = answerArray;
+    document.getElementById("hold").innerHTML = answerArray.join(" ");
     console.log(word);
 }
 
@@ -20,22 +23,68 @@ function car(){
     for (var i = 0; i < word.length; i++) {
         answerArray[i] = "_";
     }  
-    document.getElementById("hold").innerHTML = answerArray;
+    document.getElementById("hold").innerHTML = answerArray.join(" ");
     console.log(word);
-}
-for (var i = 0; i < alphabet.length; i++) {
-    var myButtons=document.getElementById("buttons");
-    var letters = document.createElement("ul");
-    letters.id="alphabet";
-    var list= document.createElement("li");
-    list.id="letter";
-    list.innerHTML=alphabet[i];
-  
-  
-  myButtons.appendChild(letters);
-  letters.appendChild(list);
+
 }
 
+
+$(document).ready(function(){
+        $("button").click(function() {
+            var value = $(this).text();
+            if(word.includes(value)){
+                answerArray.forEach(function(item, i) { 
+                    if (item) answerArray[i] = value; 
+                    document.getElementById("hold").innerHTML=answerArray,join("");
+                });
+            }
+            });
+        });
+
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.moveTo(10,20);
+ctx.lineTo(10,200);
+ctx.stroke();
+ctx.moveTo(10,20);
+ctx.lineTo(60,20);
+ctx.stroke();
+ctx.moveTo(60,20);
+ctx.lineTo(60,40);
+ctx.stroke();
+ctx.beginPath();
+ctx.arc(60,49,8,0,2*Math.PI);
+ctx.stroke();
+ctx.moveTo(60,90);
+ctx.lineTo(60, 57);
+ctx.stroke();
+ctx.moveTo(60,57);
+ctx.lineTo(85, 75);
+ctx.stroke();
+ctx.moveTo(60,57);
+ctx.lineTo(35,79);
+ctx.stroke();
+ctx.moveTo(60,90);
+ctx.lineTo(35,120);
+ctx.stroke();
+ctx.moveTo(60,90);
+ctx.lineTo(85, 120);
+ctx.stroke();
+    
+function showhappy() {
+    document.getElementById("myhDiv").style.display="block";
+    setTimeout("hideh()", 3000);  // 2 seconds
+   }
+   function hideh() {
+    document.getElementById("myhDiv").style.display="none";
+   }
+   function showsad() {
+    document.getElementById("mysDiv").style.display="block";
+    setTimeout("hides()", 3000);  // 2 seconds
+   }
+   function hides() {
+    document.getElementById("mysDiv").style.display="none";
+   }
 
 
     
